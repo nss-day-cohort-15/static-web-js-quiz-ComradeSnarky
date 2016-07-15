a818b242aa6044a380ed6018e10e85c175ef46e8
Lauren Kight <kight6@gmail.com>
2016-07-15 14:17:27 -0500

    added button, added object for inputs and event listeners

--- quiz.js
+++ quiz.js
@@ -1,7 +1,33 @@
-function() {
-
   treeObj {
-    height: (input type="text"),
-    char: (input type="text" which character to use)
+    height: document.getElementbyId("height"),
+    character: document.getElementbyId("character")
   }
-}
+
+//creates a tree w/ height and charater provided by user
+
+
+
+//Specifies character for the tree
+
+
+
+//if enter is pressed the event listener will run
+//this function to grow tree
+
+
+
+
+//Gives alert if text field is not filled in.
+//Otherwise it runs the tree program
+
+
+
+
+
+//Button event listener to grow tree
+document.getElementbyId("initializeTree").addEventListener("click", //(need function to stop if input is blank))
+
+
+//Enter event listener to grow tree
+document.getElementbyId("height").addEventListener("keypress", //need function to stop if height input is blank)
+document.getElementbyId("character").addEventListener("keypress", //need function to stop if character input is blank)
