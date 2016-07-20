c7f60a443b5a9b3a786bad11b2433eed5878558f
Lauren Kight <kight6@gmail.com>
2016-07-20 13:06:01 -0500

    finished tree build html and js

--- quiz.js
+++ quiz.js
@@ -1,33 +1,43 @@
-  treeObj {
-    height: document.getElementbyId("height"),
-    character: document.getElementbyId("character")
+// Assigning input values to an object after event validation
+  function assignInput() {
+    var treeObj = {
+      height: height.value,
+      char: character.value
+    };
+    growTree(treeObj);
   }
 
-//creates a tree w/ height and charater provided by user
 
-
-
-//Specifies character for the tree
-
-
-
-//if enter is pressed the event listener will run
 //this function to grow tree
+  function growTree(treeObj) {
+    for (var i = 1; i <= treeObj.height; i++) {
+      console.log(" ".repeat(treeObj.height - i) + treeObj.char.repeat((i * 2) - 1) + " ".repeat(treeObj.height - i))//output of tree
+    }
+  }
 
 
+// Function to check whether both input fields are completed
+  function ifEmpty() {
+    if (height.value === "" || character.value === "") {
+      alert("Please enter a height and character to build your tree");
+    } else {
+     assignInput();
+    }
+  }
+
+// Function to check whether the enter key was pressed
+function enterPressed() {
+  if (event.keyCode === 13) {
+    ifEmpty();
+  }
+}
+// //Button event listener to grow tree
+grow.addEventListener("click", ifEmpty)//(need function to stop if input is blank))
 
 
-//Gives alert if text field is not filled in.
-//Otherwise it runs the tree program
+// // //Enter event listener to grow tree
+height.addEventListener("keypress", enterPressed) //need function to stop if height input is blank)
+character.addEventListener("keypress", enterPressed) //need function to stop if character input is blank)
 
 
-
-
-
-//Button event listener to grow tree
-document.getElementbyId("initializeTree").addEventListener("click", //(need function to stop if input is blank))
-
-
-//Enter event listener to grow tree
-document.getElementbyId("height").addEventListener("keypress", //need function to stop if height input is blank)
-document.getElementbyId("character").addEventListener("keypress", //need function to stop if character input is blank)
+// create a reset button
